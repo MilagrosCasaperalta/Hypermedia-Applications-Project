001a2d053fffc6d110a2e111a53191e167b8cff6
MilagrosCasaperalta <milagrosadriana.casaperalta@mail.polimi.it>
2025-05-20 23:54:49 +0200

    Created basic contact us page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,10 +1,48 @@
 <template>
-    <div>
-     <h1>this is contact page</h1>
-  //   <TheMap></TheMap>
-    </div>
+   <section class="hero">
+      <div class="overlay"></div>
+      <div class="hero-content">
+        <h1>Contact SoulFlow NYC</h1>
+        <p>Begin your journey with us.</p>
+      </div>
+    </section>
   </template>
 
+
+<style scoped>
+
+.hero {
+  position: relative;
+  background-image: url('/img/contact/contact_us_main.jpg');
+  background-size: cover;
+  background-position: center;
+  height: 60vh;
+  overflow: hidden;
+}
+
+.hero-content {
+  position: absolute;
+  z-index: 1;
+  bottom: 40px;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+  max-width: 700px;
+  padding: 0 20px;
+}
+
+.hero-content h1 {
+  font-size: 3rem;
+  margin-bottom: 20px;
+  color: white;
+}
+
+.hero-content p {
+  font-size: 1.3rem;
+  margin-bottom: 20px;
+  color: white;
+}
+</style>
 
   
 
